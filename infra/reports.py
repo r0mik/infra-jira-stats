7e4f8a69983d9a3efaf3886ecfa046a817ff2447
rvyalov <rvyalov@mirantis.com>
2017-11-03 17:58:34 +0300

    Create new project
    Add some libs :
    * issueparser
    * jiralib
    * reports

---
+++ infra/reports.py
@@ -0,0 +1,47 @@
+
+import issueparser
+
+
+class Reports:
+    def __init__(self, jira_class):
+        self.jira=jira_class
+
+    def issue_in_statuses_in_days(self, issue, status):
+        if type(status) != int:
+            status=issueparser.status_to_int(status)
+        time = 0
+        latest_date = None
+        issue = self.jira.get_issue_by_name(issue)
+        trans = self.jira.get_issue_transitions(issue, status)
+        for i in trans:
+            for j in i:
+                if i[j][0] == status:
+                    time += int((j - latest_date).days)
+                else:
+                    latest_date = j
+
+        return time
+    def history_of_task(self, issue, status):
+        if type(status) != int:
+            status=issueparser.status_to_int(status)
+        time = 0
+        out={}
+        latest_date = None
+        issue = self.jira.get_issue_by_name(issue)
+        trans = self.jira.get_issue_transitions(issue, status)
+        for i in trans:
+            for j in i:
+                if i[j][0] == status:
+                    time += int((j - latest_date).days)
+                else:
+                    latest_date = j
+
+
+        return time
+
+    def return_epics(self):
+        out = self.jira.get_all_epic()
+        print dir(out[0].fields)
+        for i in out:
+            print i.fields.customfield_11701
+        return 0
---
+++ main.py
@@ -0,0 +1,30 @@
+# This script shows how to use the client in anonymous mode
+# against jiralib.atlassian.com.
+from infra import jiralib,issueparser,reports
+from config import Config
+from optparse import OptionParser
+
+
+
+parser = OptionParser(usage="usage: %prog [options] filename",
+                          version="%prog 1.0")
+
+parser.add_option("-c", "--config",
+                  action="store",
+                  dest="configpath",
+                  default="jira.conf",
+                  help="path to config file"
+                 )
+(options, args) = parser.parse_args()
+
+conf = Config(options.configpath)
+conf.get_access()
+conf.get_settings()
+
+myjira = jiralib.JiraLib(conf.jira_host,conf.jira_username,conf.jira_password,conf.component_name,conf.delivery_team)
+myjira.connect()
+myreport = reports.Reports(myjira)
+
+print myreport.return_epics()
+#print myreport.issue_in_statuses_in_days('PROD-1009', 'Design')
+#print myreport.issue_in_statuses_in_days('PROD-1009', 6)
